65a6318e7b2ef6a05a2695065538cd4df7e6859e
Tony Aguilar <onixag@gmail.com>
2022-08-15 23:57:05 +0200

    TEMPORARY: use events.py to test

--- examples/events.py
+++ examples/events.py
@@ -63,8 +63,21 @@ def onWarning(params: Dict[Any, Any]) -> None:
 #
 # Main code section
 #
+import logging
+
+logging.basicConfig( datefmt='%H:%M:%S',
+    level=logging.INFO,
+    format='%(asctime)s.%(msecs)03d %(levelname)-8s [%(name)s] %(message)s')
+
+log = logging.getLogger('PyATEMMax-demo')
+
+log.info("Starting")
+
 
 switcher = PyATEMMax.ATEMMax()
+
+switcher.setLogLevel(logging.DEBUG)
+switcher.setSocketLogLevel(logging.DEBUG)
 
 switcher.registerEvent(switcher.atem.events.connectAttempt, onConnectAttempt)
 switcher.registerEvent(switcher.atem.events.connect, onConnect)
@@ -73,13 +86,60 @@ switcher.registerEvent(switcher.atem.events.receive, onReceive)
 switcher.registerEvent(switcher.atem.events.warning, onWarning)
 
 switcher.connect(args.ip)
+switcher.waitForConnection()
 
-while True:
+log.info(f"mediaPlayer.stillBanks {switcher.mediaPlayer.stillBanks}")
+log.info(f"mediaPlayer.clipBanks {switcher.mediaPlayer.clipBanks}")
+
+
+print(switcher.mediaPlayer.stillFile)
+print(switcher.mediaPlayer.stillFile.itemClass)
+print(switcher.mediaPlayer.stillFile.itemDict)
+# print(switcher.mediaPlayer.stillFile._data)
+for stillBankId in switcher.mediaPlayer.stillFile._data:
+    stillBank = switcher.mediaPlayer.stillFile._data[stillBankId]
+    print(f"- [DBG] [{stillBankId}] used [{stillBank.isUsed}] name [{stillBank.fileName}]")
+'''
+'''
+
+lock_state = True
+iteration = 0
+
+while iteration < 1:
+    iteration += 1
+
     if switcher.connected:
-        print(f"[{time.ctime()}] Changing PGM")
-        switcher.setProgramInputVideoSource(0, random.randint(1,8))
 
-        print(f"[{time.ctime()}] Changing PVW")
-        switcher.setPreviewInputVideoSource(0, random.randint(1,8))
+        # print("\n"*5)
+        # print("-"*80)
+        # log.info(f"Setting LKST lock 0 to {lock_state}")
+        # switcher.sendSetLockState(0,lock_state)
 
-    time.sleep(args.interval)
+        # time.sleep(0.5)
+
+
+        print("\n"*5)
+        print("-"*80)
+        log.info(f"Acquiring Media Lock")
+        switcher.sendAcquireMediaLock(0, 2)
+
+        time.sleep(1)
+
+        switcher.sendInitDownloadToSwitcherRequest(0, 0, 2, 100)
+
+
+        # log.info(f"Requesting data transfer")
+        # switcher.sendDataTransferRequest(1,0,1)
+
+        # lock_state = not lock_state
+
+        # log.info(f"Changing PGM")
+        # switcher.setProgramInputVideoSource(0, random.randint(1,8))
+
+        # log.info(f"Changing PVW")
+        # switcher.setPreviewInputVideoSource(0, random.randint(1,8))
+
+    # time.sleep(args.interval)
+    time.sleep(5)
+
+switcher.disconnect()
